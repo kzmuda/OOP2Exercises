4a8e084105161b71711a4418c84a7f6242720508
KH <kh@wp.pl>
2021-11-28 10:53:10 +0100

    Add project files.

---
+++ OOP2Exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP2Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
